5c912ffcb23c4c34c7896b400bc143418e42fc10
Dirk <dirklawhon@protonmail.com>
2017-06-15 20:48:11 -0600

    add message form php, add skills, fix btn_banner responsive centering

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['reason']
+$message = $_POST['comments'];
+$from = 'From: dirklawhon.com';
+$to = 'dirklawhon@protonmail.com';
+$human = $_POST['human'];
+
+$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+if ($_POST['submit']) {
+    if (mail ($to, $subject, $body, $from)) {
+        echo '<p>Your message has been sent!</p>';
+    } else {
+        echo '<p>Something went wrong, try again!</p>';
+    }
+}
+
+?>
